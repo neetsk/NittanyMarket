21c367f51ddb5a4997e1176f967eccbeca71edbb
BuildTools <unconfigured@null.spigotmc.org>
2022-03-23 18:37:51 -0400

    -requirements for phase 2 demo met -added login feature

--- NittanyMarket.py
+++ NittanyMarket.py
@@ -6,15 +6,6 @@ import hashlib
 # Populates Database with starting data
 populate()
 
-'''
-connection = sql.connect('NittanyMarket.db')
-password = hashlib.sha256("test".encode('utf-8')).hexdigest()
-string = "SELECT COUNT(1) FROM Users WHERE email=? AND password=?"
-cursor = connection.execute(string, ["ijoijoifjsd","9057bc90227bb3025b8e2a4049763407678525e5165192e463c27871af3f2893"])
-print(cursor.fetchall())
-'''
-
-
 app = Flask(__name__)
 
 host = 'http://127.0.0.1:5000/'
@@ -27,145 +18,28 @@ def index():
     return render_template('index.html')
 
 
+#use args for get and form for post
 @app.route('/logincomplete', methods=['POST', 'GET'])
 def index2():
     error = None
-    data = request.args.get('User')
-    print(data)
-    if request.method == 'POST':
-        result = None
-        #result = valid_login(request.form['User'], request.form['Pass'])
-        if result:
-            return render_template('logincomplete.html')
-        else:
-            error = 'login failed'
-    return render_template('logincomplete.html', error=error)
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/x-www-form-urlencoded'):
+        if request.method == 'POST':
+            result = valid_login(request.form['userid'], request.form['password'])
+            if result[0] == 1:
+                return render_template('logincomplete.html')
+            else:
+                error = 'login failed'
+    return render_template('loginfailed.html', error=error)
+
 
 def valid_login(user, password):
     connection = sql.connect('NittanyMarket.db')
     password = hashlib.sha256(password.encode('utf-8')).hexdigest()
     string = "SELECT COUNT(1) FROM Users WHERE email=? AND password=?"
     cursor = connection.execute(string, [user, password])
-    return cursor.fetchall()
+    return cursor.fetchone()
 
 if __name__ == "__main__":
     app.run()
-
-
-
-#Drop tables so program can be reloaded
-#and duplicate values are not loaded
-#cur.execute('''DROP TABLE IF EXISTS Emp''')
-
-#Create Tables
-#cur.execute('''CREATE TABLE Emp (eid INTEGER, ename STRING, age INTEGER, salary REAL
-#    ,PRIMARY KEY (eid))''')
-
-#cur.execute('''CREATE TABLE Works (eid INTEGER, did INTEGER, pct_time INTEGER, FOREIGN KEY (eid) REFERENCES Emp)''')
-
-#cur.execute('''CREATE TABLE Dept (did INTEGER, budget REAL, managerid INTEGER
-#    ,FOREIGN KEY (did) REFERENCES Works)''')
-
-#Read data from file into list
-#dataset = open("Homework5-dataset-sp2022.txt", 'r')
-#dataList = dataset.readlines()
-
-#empList = []
-#worksList = []
-#deptList = []
-
-#empFlag = False
-#worksFlag = False
-#deptFlag = False
-
-#Parse data from lists into each view
-#for x in dataList:
-#    if empFlag and x.strip() != '':
-#        empList.append(x.strip().replace("'", ''))
-#    elif worksFlag and x.strip() != '':
-#        worksList.append(x.strip().replace("'", ''))
-#    elif deptFlag and x.strip() != '':
-#        deptList.append(x.strip().replace("'", ''))
-
-#    if "Emp" in x:
-#        empFlag = True
-#        worksList.pop()
-#    elif "Works" in x:
-#        empFlag = False
-#        worksFlag = True
-#        empList.pop()
-#    elif "Dept" in x:
-#        worksFlag = False
-#        deptFlag = True
-
-#Put items from empList into our database
-#for x in empList:
-#    tempList = x.split(',')
-#    tempList[0] = int(tempList[0])
-#    tempList[2] = int(tempList[2])
-#    tempList[3] = float(tempList[3])
-#
-#    cur.execute('''INSERT INTO Emp (eid, ename, age, salary) VALUES (?,?,?,?)''',(tempList[0],
-#        tempList[1], tempList[2], tempList[3]))
-
-#put items from worksList into our works table
-#for x in worksList:
-#    tempList = x.split(',')
-#    tempList[0] = int(tempList[0])
-#    tempList[1] = int(tempList[1])
-#    tempList[2] = int(tempList[2])
-
-#    cur.execute('''INSERT INTO Works (eid, did, pct_time) VALUES (?,?,?)''', (tempList[0],
-#        tempList[1], tempList[2]))
-
-#put items from deptList into our dept table
-#for x in deptList:
-#    tempList = x.split(',')
-#    tempList[0] = int(tempList[0])
-#    tempList[1] = float(tempList[1])
-#    tempList[2] = int(tempList[2])
-
-#    cur.execute('''INSERT INTO Dept (did, budget, managerid) VALUES (?,?,?)''', (tempList[0],
-#        tempList[1], tempList[2]))
-
-#Confirm values are in the table
-#print('Task 1')
-#cur.execute('''SELECT * FROM Emp''')
-#print(cur.fetchall())
-#cur.execute('''SELECT * FROM Works''')
-#print(cur.fetchall())
-#cur.execute('''SELECT * FROM Dept''')
-#print(cur.fetchall())
-
-#Queries for Task 2
-#Query 1
-#cur.execute('''SELECT E.ename, E.salary FROM Emp E, Works W1, Works W2
-#    WHERE E.eid = W1.eid and E.eid = W2.eid and W1.did = 0 and W2.did = 2''')
-
-#print('\n\nTask 2')
-#print('1: ', cur.fetchall())
-
-#Query 2
-#cur.execute('''SELECT E.ename, E.salary, E.age, MIN(E.eid) AS minage FROM Emp E, Works W
-#    WHERE E.eid = W.eid GROUP BY W.did''')
-#print('2: ', cur.fetchall())
-
-#Query 3
-#cur.execute('''SELECT E.ename, MAX(D.budget) AS maxbudget FROM Emp E, Works W, Dept D
-#    WHERE E.eid = W.eid and W.did = D.did and D.managerid = E.eid''')
-#print('3: ', cur.fetchall())
-
-#Query 4
-#cur.execute('''SELECT W1.did, MAX(maxbudget) FROM Works W1, (SELECT W2.did, AVG(E.salary) AS maxbudget FROM Emp E, Works W2, Dept D
-#    WHERE E.eid = W2.eid and W2.did = D.did GROUP BY W2.did)''')
-#print('4: ', cur.fetchall())
-
-#Query 5
-#cur.execute('''SELECT DISTINCT E1.ename, E1.salary FROM Emp E1, Works W1, (SELECT E2.ename, MIN(E2.salary) as minmanagersalary FROM Emp E2, Works W2, Dept D
-#    WHERE E2.eid = W2.eid and W2.did = D.did and D.managerid = E2.eid) WHERE E1.eid = W1.eid and E1.salary > minmanagersalary''')
-#print('5: ', cur.fetchall())
-
-#close file and commit changes to db
-#dataset.close()
-#con.commit()
 
