82cd5f7bdeb62af6786d5fd52ad3493ac24b39cd
BuildTools <unconfigured@null.spigotmc.org>
2022-04-24 21:13:57 -0400

    updated profile page

--- NittanyMarket.py
+++ NittanyMarket.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import sqlite3 as sql
 from populateDatabase import populate
 import hashlib
+import secrets
 
 # Populates Database with starting data
 populate()
 
 app = Flask(__name__)
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SECRET_KEY"] = secrets.token_urlsafe(16) 
 
 host = 'http://127.0.0.1:5000/'
 
 con = sql.connect("NittanyMarket.db")
+
+#resources: sessions: https://testdriven.io/blog/flask-sessions/
+
+#RUN APPLICATION inside | lines || & C:/Python310/python.exe c:/Users/minij/Desktop/NittanyMarket/NittanyMarket.py||
 
 #TEST LOGIN
 #User: arubertelli0@nsu.edu
@@ -38,21 +45,51 @@ def index2():
     return render_template('loginfailed.html', error=error) #we didnt have a password and user that match so we fail login with error=loginfailed
 
 
-@app.route('/profile', methods=['GET'])
+#@app.route('/profile/<username>, methods=['GET','POST']')
+@app.route('/profile', methods=['GET', 'POST'])
 def index3():
     #if user not logged in send them to login
-    if (False):
-        None=None
+    
     #else display user profile
-    else: 
-        #fetch profile data
-        data = fetch_profile_data()
-        #pass data under return
-    return render_template('profile.html', data=data)
+    
+    #fetch profile data
+    
+    data = fetch_profile_data()
+    if request.method == 'GET':
+        test = None
+    else:
+        #if it worked
+        changePassAttempt = True
+        #if change failed
+        #changePassAttempt = False
+    #pass data under return
+    
+    return render_template('profile.html', data=data, changePassAttempt=changePassAttempt)
 
 
 def fetch_profile_data():
-    return True
+    tempUser = 'arubertelli0@nsu.edu'
+    #tempPass = hashlib.sha256('TbIF16hoUqGl'.encode('utf-8')).hexdigest()
+    connection = sql.connect('NittanyMarket.db')
+    
+    string = "SELECT * FROM Buyers WHERE email=?"
+    cursor = connection.execute(string, [tempUser])
+    tempdata = cursor.fetchone()
+    # example result ('arubertelli0@nsu.edu', 'Ileana', 'Ziehms', 'Female', 49, 
+    # 'bbeb14f144684b76b5322bdee24f8c76', '63f97e32f7894bd0bc43d113a431cb9b')
+
+    string = "SELECT zipcode, street_num, street_name FROM Address WHERE address_id=?"
+    cursor = connection.execute(string, [tempdata[5]])
+    temphomeadd = cursor.fetchone()
+
+    cursor = connection.execute(string, [tempdata[6]])
+    tempbillingadd = cursor.fetchone()
+
+    string = "SELECT SUBSTRING(credit_card_num, 16, 4) FROM Credit_Cards WHERE Owner_email=?"
+    cursor = connection.execute(string, [tempUser])
+    lastfourdigit = cursor.fetchone()
+
+    return tempdata + temphomeadd + tempbillingadd + lastfourdigit
 
 
 def valid_login(user, password):
